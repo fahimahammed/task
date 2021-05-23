f0231775946e16ebabcc22bb477916ad47880f4d
fahimahammed <fahimahammed.cse@gmail.com>
2021-05-23 10:56:26 +0600

    some update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import * as XLSX from "xlsx";
 import { useState } from 'react';
 import Row from './components/Row';
@@ -7,16 +6,19 @@ function App() {
   const [items, setItems] = useState([]); // state for set json data(converted from excel file)
 
   const readExcel=(file)=>{
-    console.log('file1',file);
+    //console.log('file:: ',file);
+
     const promise = new Promise((resolve, reject) =>{
       const fileReader = new FileReader();
       fileReader.readAsArrayBuffer(file);
 
       fileReader.onload = (e) => {
         const bufferArray = e.target.result;
+
         const wb = XLSX.read(bufferArray, { type: "buffer" });
         const wsname = wb.SheetNames[0];
         const ws = wb.Sheets[wsname];
+
         const data = XLSX.utils.sheet_to_json(ws); //convert excel file to json
         resolve(data);
       };
@@ -28,24 +30,22 @@ function App() {
 
     promise.then((data) => {
       setItems(data);   // set data in state
+      //console.log('data:: ', data);
     });
     
   };
 
       
-  
-
   return (
     <section className='p-2'>
-        
+        <lebel>Upload provided xlsx file: </lebel>
         <input type='file'
           onChange={(e) => {
             const file = e.target.files[0];
             readExcel(file);
         }}/>
         
-        { items && <h1>{items.length}</h1>}
-        <table className="table table-striped table-hover table-bordered table-sm border-secondary">
+        <table className="table table-striped table-hover table-bordered table-sm border-secondary mt-5">
           <thead>
             <tr className='text-center bg-secondary bg-gradient text-white'>
               <th scope="col">instrument_token</th>
@@ -62,10 +62,11 @@ function App() {
               <th scope="col">exchange</th>
             </tr>
           </thead>
+
           <tbody>
             { items.map((data) => <Row key={data.instrument_token} data={data}></Row>)}
-           
           </tbody>
+
         </table>
     </section>
   );
--- src/components/Row.js
+++ src/components/Row.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Row = ({data}) => {
     const {instrument_token, exchange_token, tradingsymbol, name, last_price, expiry, strike, tick_size, lot_size, instrument_type, segment, exchange} = data;
-    const date = new Date(Math.round((expiry - 25569)*86400*1000));
     
+    const date = new Date(Math.round((expiry - 25569)*86400*1000));
     let formattedDate = date.getDate() + "/"+ parseInt(date.getMonth()+1) +"/"+date.getFullYear();
-    //console.log(date2)
+    
+    //console.log(formattedDate);
     // console.log(date);
     
     return (
@@ -17,8 +18,8 @@ const Row = ({data}) => {
                 <td>{name}</td>
                 <td>{last_price}</td>
                 <td>{formattedDate}</td>
-                <td style={{backgroundColor: strike>=80 ? 'green' : 'red'}}>{strike}</td>
-                <td style={{backgroundColor: tick_size<=0.0025 ? 'green' : 'red'}}>{tick_size}</td>
+                <td style={{backgroundColor: strike>=80 ? strike == 0 ? 'none' : 'green' : 'red'}}>{strike}</td>
+                <td style={{backgroundColor: tick_size<=0.0025 ? tick_size==0 ? 'none' : 'green' : 'red'}}>{tick_size}</td>
                 <td>{lot_size}</td>
                 <td>{instrument_type}</td>
                 <td>{segment}</td>
